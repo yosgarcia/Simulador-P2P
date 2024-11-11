26dfd6fe8638a3d59a418807bb70d6819c3531ef
Santiago <santiramos.cr@gmail.com>
2024-11-11 16:54:38 -0600

    primero

---
+++ servidor.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+
+struct FileInfo{
+    string hash1;
+    string hash2;
+    size_t size;
+    string ip;
+    int port;
+}
+
+
+map<string, vector<FileInfo>> fileDatabase;
+
+int main(){
+
+    return 0;
+}
